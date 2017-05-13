e97828fdb24b8a228aa3956e47a04251dc54f2ea
Homing <470034235@qq.com>
2017-05-13 14:47:35 +0800

    update numpy_project

--- python/numpy_project.py
+++ python/numpy_project.py
@@ -16,10 +16,10 @@ import numpy as np
 def traverse_file(path):
     l = []
     for root, dirs, files in os.walk(path):
-        files=filter(lambda file:file[-4:]=='.mp3',files)
+        files = filter(lambda file: file[-4:] == '.mp3', files)
         for fn in files:
-            file = root + "\\" + fn  # 注意是两个斜杠
-            l.append(file)
+            file1 = root + "\\" + fn  # 注意是两个斜杠
+            l.append(file1)
     return l
 
 
@@ -30,14 +30,12 @@ def play_music_random(path):
     time.sleep(120)
 
 
-
-
 def main():
     path = r'D:\CloudMusic'
     playlist1 = traverse_file(path)
     for i in np.arange(len(playlist1)):
         t = playlist1[np.random.randint(0, len(playlist1))]
-        print("播放音乐...%s" % t)
+        print("...播放音乐...\n %s" % t)
         play_music_random(t)
 
 if __name__ == '__main__':
