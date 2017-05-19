4c823bdfcc543246aa962e84dcd17c57c56150a2
Homing <470034235@qq.com>
2017-05-19 20:27:35 +0800

    update wordcloud

--- python/my_word_cloud.py
+++ python/my_word_cloud.py
@@ -12,28 +12,27 @@ from os import path
 import matplotlib.pyplot as plt
 import random
 import os
+from matplotlib import font_manager
 
 from wordcloud import WordCloud, STOPWORDS
 
 
-font=os.path.join(os.path.dirname(__file__), "DroidSansFallbackFull.ttf")
+font = "DroidSansFallbackFull.ttf"
+zhfont1 = font_manager.FontProperties(fname='DroidSansFallbackFull.ttf')  # 中文配置字体
 
 def grey_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
-d = path.dirname(__file__)
+mask = np.array(Image.open(r'D:\Big_data\大数据学习\data_science_way\python\04.png'))
 
 
-mask = np.array(Image.open(path.join(d, "stormtrooper_mask.png")))
-#mask = np.array(Image.open(path.join(d, "04.png")))
-
-
-text = open(u"santi.txt").read().decode('gbk')
+text = open(r"D:\Big_data\大数据学习\data_science_way\python\santi.txt").read()
 
 # preprocessing the text a little bit
-text = text.replace(u"程心说", u"程心")
-text = text.replace(u"程心和", u"程心")
-text = text.replace(u"程心问", u"程心")
+text = text.replace("程心说", "程心")
+text = text.replace("程心和", "程心")
+text = text.replace("程心问", "程心")
+text = text.replace("程心*", "程心")
 
 # adding movie script specific stopwords
 stopwords = set(STOPWORDS)
@@ -46,10 +45,11 @@ wc = WordCloud(font_path=font,max_words=2000, mask=mask, stopwords=stopwords, ma
 default_colors = wc.to_array()
 plt.title("Custom colors")
 plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3))
-wc.to_file("a_new_hope.png")
+wc.to_file("b_new_hope.png")
 plt.axis("off")
 plt.figure()
-plt.title(u"三体-词频统计")
+plt.title("三体-词频统计", fontproperties=zhfont1)  # 需配置字体，不然会中文显示乱码
 plt.imshow(default_colors)
 plt.axis("off")
+plt.savefig('b_new_hope1.png')
 plt.show()
