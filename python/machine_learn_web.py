cb757d8d7dc0af00179cc5f1897fc66bdcb6988b
Homing <470034235@qq.com>
2017-05-23 23:19:53 +0800

    machine website

---
+++ python/machine_learn_web.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Homing
+@software: PyCharm Community Edition
+@file: machine_learn_web.py
+@time: 2017/5/23 23:10
+"""
+#  CSDN 机器学习
+#  http://lib.csdn.net/base/machinelearning
+
+
+
+
+
+
+
+
+
+
