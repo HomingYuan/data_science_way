a3b36de211e088af54a53a09fbe76d514bf400c4
Homing <470034235@qq.com>
2017-06-26 15:42:46 +0800

    add house_price

---
+++ python/numpy_project.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Homing
+@software: PyCharm Community Edition
+@file: numpy_project.py
+@time: 2017/5/9 20:08
+"""
+
+import os
+import pygame
+import time
+import numpy as np
+
+
+def traverse_file(path):
+    l = []
+    for root, dirs, files in os.walk(path):
+        files = filter(lambda file: file[-4:] == '.mp3', files)
+        for fn in files:
+            file1 = root + "\\" + fn  # 注意是两个斜杠
+            l.append(file1)
+    return l
+
+
+def play_music_random(path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:  # 实现播放完后，播放下一首
+        pass
+
+
+def main():
+    path = r'D:\CloudMusic'
+    playlist1 = traverse_file(path)
+    for i in np.arange(len(playlist1)):
+        t = playlist1[np.random.randint(0, len(playlist1))]
+        print("...播放音乐...\n %s" % t)
+        play_music_random(t)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
