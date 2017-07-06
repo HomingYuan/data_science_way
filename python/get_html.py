19b896e5bf9a13b051b70fe8297472c4d578445c
Homing <470034235@qq.com>
2017-07-06 23:10:34 +0800

    update job_data

--- python/get_html.py
+++ python/get_html.py
@@ -11,11 +11,8 @@ from bs4 import BeautifulSoup
 import urllib.parse
 import pymysql
 from datetime import datetime
-<<<<<<< HEAD
-=======
 
 
->>>>>>> f11ccded1403378631ed77b09dbacf85897c7814
 # 链接到mysql
 connection = pymysql.connect(host='127.0.0.1',
                              port=3306,
@@ -51,13 +48,13 @@ for i in job_list:
         connection.rollback()
 
 def main():
-<<<<<<< HEAD
+
     now = datetime.now()
     print(now)
-=======
+
     start = datetime.now()
     print(start)
->>>>>>> f11ccded1403378631ed77b09dbacf85897c7814
+
     for kw in kws:
         for page in range(1, 100):
             try:
@@ -78,16 +75,16 @@ def main():
             if i % 5 == 4:
                 f.write(job_list[i])
                 f.write('\n')
-<<<<<<< HEAD
+
     end = datetime.now()
     print('程序运行%s'%(str(end-now)))
 
-=======
+
 
     end = datetime.now()
     print(end)
     print("程序耗时： " + str(end - start))
->>>>>>> f11ccded1403378631ed77b09dbacf85897c7814
+
 if __name__ == '__main__':
     main()
 
